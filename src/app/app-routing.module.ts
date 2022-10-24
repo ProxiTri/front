0fd28937f7e9a6c11a1a3c5a909d3692af64f49c
maxencelgy <maxencelgy@gmail.com>
2022-10-24 16:23:30 +0200

    route map

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {HomepageComponent} from "./homepage/homepage.component";
 import {LoginComponent} from "./login/login.component";
+import {MapComponent} from "./map/map.component";
 import {MenuComponent} from "./header/menu/menu.component";
 import {LogoComponent} from "./header/menu/logo/logo.component";
 import {NavigationComponent} from "./header/menu/navigation/navigation.component";
@@ -9,14 +10,26 @@ import {ArticlesComponent} from "./homepage/articles/articles.component";
 import {SliderComponent} from "./homepage/slider/slider.component";
 
 const routes: Routes = [
+  // HOME
   {
     path: '',
     component: HomepageComponent
   },
+
+  // LOGIN
   {
     path: 'login',
     component: LoginComponent
+  },
+
+  // MAP
+  {
+    path: 'map',
+    component: MapComponent
   }
+
+
+
 ];
 
 @NgModule({
