4fedea5a97fc6b753ce026d13bec30e929031e78
BrietGame <alexis.briet2003@gmail.com>
2022-12-07 09:33:12 +0100

    Fix Module : delete forever ngx-barcode

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -25,7 +25,6 @@ import { LoginComponent } from './login/login.component';
 import { InfosComponent } from './pollution/infos/infos.component';
 import { FooterComponent } from './footer/footer.component';
 import { DemoDirective } from './demo.directive';
-import { NgxBarcodeModule } from 'ngx-barcode';
 import {BarcodeScannerLivestreamModule} from "ngx-barcode-scanner";
 import { RegisterComponent } from './register/register.component';
 import { CommuneComponent } from './commune/commune.component';
@@ -61,7 +60,6 @@ import { CommuneComponent } from './commune/commune.component';
     AppRoutingModule,
     HttpClientModule,
     BarcodeScannerLivestreamModule,
-    NgxBarcodeModule,
     FormsModule,
     ReactiveFormsModule
   ],
