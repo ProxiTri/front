febb126327f5897db4ccd0916ac88f898a4614a8
maxencelgy <maxencelgy@gmail.com>
2022-12-07 09:54:22 +0100

    Form de contact envoie de mail

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-contact',
@@ -6,10 +8,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent implements OnInit {
-
-  constructor() { }
+  // @ts-ignore
+  form: FormGroup;
 
   ngOnInit(): void {
+    this.buildForm();
   }
 
+  constructor(private formBuilder: FormBuilder, private http: HttpClient) {}
+
+  send(): void {
+    const { sujet, email, message } = this.form.value;
+
+    const headers = new HttpHeaders({ "Content-Type": "application/json" });
+    this.http.post(
+      "https://formsubmit.co/maxencelgy@gmail.com",
+      { replyto: email, email: email, subject: sujet, message: message},
+      { headers: headers }
+    )
+      .subscribe(response => {
+        console.log(response);
+      });
+
+
+    alert(`Sujet: ${sujet}, Email: ${email}, Message: ${message}`);
+  }
+
+  private buildForm(): void {
+    this.form = this.formBuilder.group({
+      sujet: this.formBuilder.control(null),
+      email: this.formBuilder.control(null),
+      message: this.formBuilder.control(null),
+    });
+  }
 }
