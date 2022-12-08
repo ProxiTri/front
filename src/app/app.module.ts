c46cc686d9f284308d28f27de3ae1e58ee69cf2f
BrietGame <alexis.briet2003@gmail.com>
2022-12-08 11:18:31 +0100

    HotFix : delete missing files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,6 @@ import { AccountComponent } from './account/account.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { SliderComponent } from './homepage/slider/slider.component';
 import { GainsComponent } from './homepage/gains/gains.component';
-import { ArticlesComponent } from './homepage/articles/articles.component';
 import { ActuComponent } from './account/actu/actu.component';
 import { AstucesComponent } from './account/astuces/astuces.component';
 import { ChatComponent } from './account/chat/chat.component';
@@ -41,7 +40,6 @@ import { ContactComponent } from './contact/contact.component';
     HomepageComponent,
     SliderComponent,
     GainsComponent,
-    ArticlesComponent,
     ActuComponent,
     AstucesComponent,
     ChatComponent,
