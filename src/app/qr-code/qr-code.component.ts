1a2ed96e7a48e92e0e209f5971e0961c82f035d7
selim27000 <selim27000@hotmail.com>
2022-12-09 10:09:16 +0100

    fix error hendler

--- src/app/qr-code/qr-code.component.ts
+++ src/app/qr-code/qr-code.component.ts
@@ -6,6 +6,7 @@ import AOS, {refresh} from "aos";
 import {FormBuilder, Validators} from "@angular/forms";
 import { Router } from '@angular/router';
 import { UpperCasePipe } from '@angular/common';
+import { exit } from "process";
 
 
 
@@ -19,11 +20,17 @@ export class QrCodeComponent implements AfterViewInit {
   @ViewChild(BarcodeScannerLivestreamComponent)
   barcodeScanner: BarcodeScannerLivestreamComponent | undefined;
 
+  test:any;
   state: any = {};
   qrCode: any;
   data: any
   product: any;
   productAr: any;
+  togglePla = true;
+  toggleOm = true;
+  toggleVerre = true;
+  togglePapier = true;
+  toggle9 = true;
   toggle8 = true;
   toggle7 = true;
   toggle6 = true;
@@ -35,7 +42,7 @@ export class QrCodeComponent implements AfterViewInit {
   audio: any;
   AOS: any;
   isSubmitted = false;
-  Condit: any = ['Plastique', 'Verre', 'Papier', 'Ordure Ménagères'];
+  Condit: any = ['PLASTIQUE', 'VERRE', 'PAPIER', 'ORDURES MENAGERES'];
   categoryName: any;
   name: any;
   productCode: any;
@@ -43,13 +50,11 @@ export class QrCodeComponent implements AfterViewInit {
 
 
   ngAfterViewInit() {
-this.toggle8 = false;
 new Promise((resolve, reject) => {
       // @ts-ignore
       resolve(this.barcodeScanner.start())
     }).then(() => {
       this.toggle4 = false;
-
     })
     AOS.init();
   }
@@ -70,20 +75,50 @@ new Promise((resolve, reject) => {
       this.barcodeScanner.stop();
 
       this.productAr = {
-        marque: this.product.brands ?? null,
-        packaging: this.product.packaging ?? null,
-        image: this.product.image_front_small_url ?? null
+        marque: this.product.brands || "",
+        packaging: this.product.packaging || "",
+        image: this.product.image_front_small_url || ""
       }
-      if (this.productAr.packaging == null || this.productAr.packaging == "") {
+
+      if ( this.productAr.packaging == "") {
         this.toggle5 = true;
+        
       } else {
         this.toggle5 = false;
       }
-      if (this.productAr.brands == "") {
+      if (this.productAr.marque == "") {
         this.toggle6 = false;
       }
       if (this.productAr == null) {
         this.toggle3 = true;
+      }
+      if(this.productAr.packaging.indexOf('plastique') > -1 || this.productAr.packaging.indexOf('Plastique') > -1 || this.productAr.packaging.indexOf('PLASTIQUE') > -1 ){
+        this.uppercaseParams = "PLASTIQUE"
+        this.togglePla = true;
+        this.toggleVerre = false;
+        this.togglePapier = false;
+        this.toggleOm = false;
+
+      }else if(this.productAr.packaging.indexOf('verre') > -1 || this.productAr.packaging.indexOf('Verre') > -1 || this.productAr.packaging.indexOf('VERRE') > -1){
+        this.uppercaseParams = "VERRE"
+        this.toggleVerre = true;
+        this.togglePla = false;
+        this.togglePapier = false;
+        this.toggleOm = false;
+
+      }else if(this.productAr.packaging.indexOf('papier') > -1 || this.productAr.packaging.indexOf('Papier') > -1 || this.productAr.packaging.indexOf('PAPIER') > -1 || this.productAr.packaging.indexOf('Papiers') > -1 || this.productAr.packaging.indexOf('papiers') > -1 || this.productAr.packaging.indexOf('PAPIERS') > -1){
+        this.uppercaseParams = "PAPIER"
+        this.togglePapier = true;
+        this.togglePla = false;
+        this.toggleVerre = false;
+        this.toggleOm = false;
+      }else{
+        this.uppercaseParams = "ORDURES MENAGERES"
+        console.log("OM")
+        this.toggleOm = true;
+        this.togglePla = false;
+        this.toggleVerre = false;
+        this.togglePapier = false;
       }
     });
   }
@@ -95,7 +130,36 @@ new Promise((resolve, reject) => {
   });
 
   changeCategory(event: any) {
-    console.log(event.target.value)
+    if(event.target.value.indexOf('plastique') > -1 || event.target.value.indexOf('Plastique') > -1 || event.target.value.indexOf('PLASTIQUE') > -1 ){
+      this.togglePla = true;
+      this.toggleVerre = false;
+      this.togglePapier = false;
+      this.toggleOm = false;
+
+    }else if(event.target.value.indexOf('verre') > -1 || event.target.value.indexOf('Verre') > -1 || event.target.value.indexOf('VERRE') > -1){
+      this.toggleVerre = true;
+      this.togglePla = false;
+      this.togglePapier = false;
+      this.toggleOm = false;
+
+    }else if(event.target.value.indexOf('papier') > -1 || event.target.value.indexOf('Papier') > -1 || event.target.value.indexOf('PAPIER') > -1 || event.target.value.indexOf('Papiers') > -1 || event.target.value.indexOf('papiers') > -1 || event.target.value.indexOf('PAPIERS') > -1){
+      this.togglePapier = true;
+      this.togglePla = false;
+      this.toggleVerre = false;
+      this.toggleOm = false;
+    }else{
+      this.uppercaseParams = "ORDURES MENAGERES"
+      this.toggleOm = true;
+      this.togglePla = false;
+      this.toggleVerre = false;
+      this.togglePapier = false;
+    }
+
+
+
+
+
+
     this.categoryName?.setValue(event.target.value, {
       onlySelf: true,
     });
@@ -119,17 +183,6 @@ new Promise((resolve, reject) => {
     location.reload()
   }
 
-  onSubmit(): void {
-    this.uppercaseParams = this.upperCasePipe.transform(this.codebarForm.value.categoryName)
-    this.router.navigate(
-      ['/map'],
-      { queryParams: { search:  this.uppercaseParams } }
-    );
-    if (!this.codebarForm.valid) {
-      false;
-    }
-  }
-
   ngOnInit(): void {
     this.productAr = null;
   }
@@ -141,40 +194,75 @@ new Promise((resolve, reject) => {
       this.toggle7 = true;
     }
     this.httpClient.get('https://world.openfoodfacts.org/api/v2/search?code=' + this.productCode).subscribe((data: any) => {
-      if (data.products.length != 0) {
-        this.audio = new Audio();
-        this.audio.src = "../assets/Apple-Sound.mp4";
-        this.audio.load();
-        this.audio.play();
-        this.product = data.products[0];
-        this.toggle3 = false;
-      }
-      // @ts-ignore
-      this.barcodeScanner.stop();
+      try {
+        if (data.products.length != 0) {
+          this.toggle9 = true;
+
+          this.audio = new Audio();
+          this.audio.src = "../assets/Apple-Sound.mp4";
+          this.audio.load();
+          this.audio.play();
+          this.product = data.products[0];
+          this.toggle3 = false;
+        
+        // @ts-ignore
+        this.barcodeScanner.stop();
 
       this.product = data.products[0];
       this.productAr = {
-        marque: this.product.brands,
-        packaging: this.product.packaging,
-        image: this.product.image_front_small_url
+      id:this.product._id,
+      count: this.product.count ,
+      marque: this.product.brands,
+      packaging: this.product.packaging,
+      image: this.product.image_front_small_url
+  }
+        }else {
+          this.toggle9 = false;
+        }
+      }catch(err){
+        console.log(err)
       }
-
     });
-
-    if (this.productAr == null) {
-      this.toggle3 = true;
-    } else {
-      this.toggle3 = false;
-    }
 
   }
 
-displayBin(data:any){
-console.log(data.indexOf('Métal') > -1);
-console.log(data.indexOf('Plastique') > -1);
-console.log(this.data)
+displayBin(){
+  if (!this.product.packaging) {
+    this.uppercaseParams = this.upperCasePipe.transform(this.codebarForm.value.categoryName)
+    this.router.navigate(
+      ['/map'],
+      { queryParams: { search:  this.uppercaseParams} }
+    );
+    if (!this.codebarForm.valid) {
+      false;
+    }
+  }
 
-
+  if(this.productAr.packaging.indexOf('plastique') > -1 || this.productAr.packaging.indexOf('Plastique') > -1 || this.productAr.packaging.indexOf('PLASTIQUE') > -1 ){
+    this.uppercaseParams = "PLASTIQUE"
+    this.router.navigate(
+      ['/map'],
+      { queryParams: { search:  this.uppercaseParams} }
+    );
+  }else if(this.productAr.packaging.indexOf('verre') > -1 || this.productAr.packaging.indexOf('Verre') > -1 || this.productAr.packaging.indexOf('VERRE') > -1){
+    this.uppercaseParams = "VERRE"
+    this.router.navigate(
+      ['/map'],
+      { queryParams: { search:  this.uppercaseParams} }
+    );
+  }else if(this.productAr.packaging.indexOf('papier') > -1 || this.productAr.packaging.indexOf('Papier') > -1 || this.productAr.packaging.indexOf('PAPIER') > -1 || this.productAr.packaging.indexOf('Papiers') > -1 || this.productAr.packaging.indexOf('papiers') > -1 || this.productAr.packaging.indexOf('PAPIERS') > -1){
+    this.uppercaseParams = "PAPIER"
+    this.router.navigate(
+      ['/map'],
+      { queryParams: { search:  this.uppercaseParams} }
+    );
+  }else{
+    this.uppercaseParams = "ORDURES MENAGERES"
+    this.router.navigate(
+      ['/map'],
+      { queryParams: { search:  this.uppercaseParams} }
+    );
+  }
 }
 
 }
